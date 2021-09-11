92f682507f893dc1d407b2f2b9c7d3dc6c84dc5f
walterdiazdev <mail@walterdiaz.dev>
2021-09-10 22:13:10 -0300

    20210909 2100

--- app/app.py
+++ app/app.py
@@ -12,5 +12,24 @@ def index():
     }
     return render_template('index.html', data=data)
 
+@app.route('/diccionarios.html')
+def diccionarios():
+    data = {
+        'nombre': 'Claudia', 
+        'edad': 25,
+        'trabaja': True
+    }
+    return render_template('diccionarios.html', data=data)
+
+@app.route('/listas.html')
+def listas():
+    data = ['Daniel','Claudia','Jorge','Patricia']
+    return render_template('listas.html', data=data)
+
+@app.route('/contacto/<nombre>')
+def contacto(nombre):
+    data = nombre
+    return render_template('contacto.html', data=data)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
